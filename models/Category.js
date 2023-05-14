de03db666b22be85aa9cf84708ebe1a0c6f1342c
JJ <jenniferj1796@gmail.com>
2023-05-13 20:45:07 -0400

    added columns for category.js in model based off readme requirements

--- models/Category.js
+++ models/Category.js
@@ -4,9 +4,22 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
+// Table name is category
 Category.init(
   {
-    // define columns
+    // This is a column in the category table 
+    id: {
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // This is a column in the category table 
+    // category_name is given in the category-seeds.js file
+    category_name: {
+      type:DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
